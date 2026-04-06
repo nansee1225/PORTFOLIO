b527677762608de1c2a538720e5edf61c6325e7c
Nansee Gupta <ng7905@srmist.edu.in>
2026-04-06 23:19:48 +0530

    h

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const toggleBtn = document.getElementById("theme-toggle");
 
-// default theme
-document.body.classList.add("dark");
-
 // load saved theme
 if (localStorage.getItem("theme")) {
   document.body.className = localStorage.getItem("theme");
@@ -12,10 +9,11 @@ toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   document.body.classList.toggle("light");
 
-  // save theme
   if (document.body.classList.contains("dark")) {
     localStorage.setItem("theme", "dark");
+    toggleBtn.textContent = "🌙";
   } else {
     localStorage.setItem("theme", "light");
+    toggleBtn.textContent = "☀️";
   }
 });
